e56cf411539338e24cb5608a9f06fd047c43afee
Luiz Bills <luizpbills@gmail.com>
2018-04-28 11:34:02 -0300

    update dist

--- build/fixed-game-loop.js
+++ build/fixed-game-loop.js
@@ -1,4 +1,7 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Timer=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Timer = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports = require('./src/fixed-game-loop.js');
+
+},{"./src/fixed-game-loop.js":3}],2:[function(require,module,exports){
 (function (global){
 // return the current time in milliseconds
 var d = global.Date;
@@ -8,7 +11,7 @@ module.exports = d.now || function () {
 };
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var now = require('./date-now'),
   raf = require('./raf');
 
@@ -20,6 +23,8 @@ module.exports = Timer;
 function Timer(options) {
   this._initialized = false;
 
+  this._curTime = 0;
+  this._frametime = 0;
   this._fixedDeltaTime = 1000 / 60;
   this._fixedDeltaTimeInSeconds = this._fixedDeltaTime / 1000;
   this._FRAME_TIME_MAX = 250;
@@ -109,11 +114,11 @@ Timer.prototype = {
 };
 
 function tick() {
-  var curTime = now();
-  var frameTime = curTime - this._prevTime;
+  this._curTime = now();
+  this._frameTime = this._curTime - this._prevTime;
 
-  if (frameTime > this._FRAME_TIME_MAX) {
-    frameTime = this._FRAME_TIME_MAX;
+  if (this._frameTime > this._FRAME_TIME_MAX) {
+    this._frameTime = this._FRAME_TIME_MAX;
   }
 
   this._prevTime = curTime;
@@ -131,7 +136,7 @@ function tick() {
   this._requestID = raf.request(this._tick);
 }
 
-},{"./date-now":1,"./raf":3}],3:[function(require,module,exports){
+},{"./date-now":2,"./raf":4}],4:[function(require,module,exports){
 (function (global){
 /** requestAnimationFrame polyfill
  * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
@@ -152,14 +157,17 @@ for (; x < l; ++x) {
 
 if (!requestAnimationFrame || !cancelAnimationFrame) {
   var now = require('./date-now'),
-    lastTime = 0, max = Math.max;
+      currTime = 0,
+      timeToCall = 0,
+      id = null,
+      lastTime = 0;
 
   requestAnimationFrame = function(callback, element) {
-    var currTime = now(),
-      timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-      id = global.setTimeout(function () {
-        callback(currTime + timeToCall);
-      }, timeToCall);
+    currTime = now();
+    timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    id = global.setTimeout(function () {
+      callback(currTime + timeToCall);
+    }, timeToCall);
     lastTime = currTime + timeToCall;
     return id;
   };
@@ -173,8 +181,5 @@ module.exports.request = requestAnimationFrame.bind(global);
 module.exports.cancel = cancelAnimationFrame.bind(global);
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./date-now":1}],4:[function(require,module,exports){
-module.exports = require('./src/fixed-game-loop.js');
-
-},{"./src/fixed-game-loop.js":2}]},{},[4])(4)
+},{"./date-now":2}]},{},[1])(1)
 });
