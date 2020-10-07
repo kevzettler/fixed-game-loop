4b771389481ef92415bf99e5b59427b5a9fc7218
Kev Zettler <kevzettler@gmail.com>
2020-10-07 16:30:19 -0700

    Use battle tested RAF
    This seems to fix requestAnimationFrame on browser clients and workers
    running faster than the setTimeout implementation. I suspect the
    previous setTimeout polyfill was not correct and this robust raf
    module fixes that.

--- src/raf.js
+++ src/raf.js
@@ -1,41 +1,4 @@
-/** requestAnimationFrame polyfill
- * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
- * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- */
-var vendors = ['ms', 'moz', 'webkit', 'o'],
+var raf = require('raf');
 
-  requestAnimationFrame = global.requestAnimationFrame,
-  cancelAnimationFrame = global.cancelAnimationFrame,
-
-  x = 0, l = vendors.length;
-
-for (; x < l; ++x) {
-  if (requestAnimationFrame && cancelAnimationFrame) break;
-  requestAnimationFrame = global[vendors[x] + 'RequestAnimationFrame'];
-  cancelAnimationFrame = global[vendors[x] + 'CancelAnimationFrame'] || global[vendors[x] + 'CancelRequestAnimationFrame'];
-}
-
-if (!requestAnimationFrame || !cancelAnimationFrame) {
-  var now = require('./date-now'),
-      currTime = 0,
-      timeToCall = 0,
-      id = null,
-      lastTime = 0;
-
-  requestAnimationFrame = function(callback, element) {
-    currTime = now();
-    timeToCall = Math.max(0, 16 - (currTime - lastTime));
-    id = global.setTimeout(function () {
-      callback(currTime + timeToCall);
-    }, timeToCall);
-    lastTime = currTime + timeToCall;
-    return id;
-  };
-
-  cancelAnimationFrame = function (id) {
-    global.clearTimeout(id);
-  };
-}
-
-module.exports.request = requestAnimationFrame.bind(global);
-module.exports.cancel = cancelAnimationFrame.bind(global);
+module.exports.request = raf;
+module.exports.cancel = raf.cacnel
